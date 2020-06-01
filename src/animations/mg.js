6d11745a52d4235cb6f6ae83744e6a44b8a26f0c
Phillip Nguyen <nh273@cornell.edu>
2020-06-01 17:52:54 +0200

    changing to vertical orientation

--- src/animations/mg.js
+++ src/animations/mg.js
@@ -2,5 +2,11 @@ import { filledMg } from "../params/patterns";
 import { decompress } from "../helpers/compact";
 
 export function loop() {
+  let arr = [];
+  for (let i in filledMg) {
+    if (filledMg[i] === 1) {
+      arr.push(+i);
+    }
+  }
   return decompress(filledMg);
 }
