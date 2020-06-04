0d66e0c052f801f74b98270aa9f45b8296f92ac2
Phillip Nguyen <nh273@cornell.edu>
2020-06-04 12:16:42 +0200

    toggleable squares

--- src/designer/patternDesigner.js
+++ src/designer/patternDesigner.js
@@ -4,6 +4,7 @@ import { w, h, dim, pad, side, _boustro } from "../helpers/helpers";
 console.log("loading module");
 
 export function main() {
+  const lightBlue = "#B5D3E7";
   let svg = d3
     .select("#d3-div")
     .append("svg")
@@ -18,7 +19,7 @@ export function main() {
     .data(d3.range(w * h))
     .enter()
     .append("g")
-    .attr("class", "design-square");
+    .attr("class", "design-square unfilled");
 
   grid
     .append("rect")
@@ -30,7 +31,7 @@ export function main() {
     })
     .attr("width", side)
     .attr("height", side)
-    .attr("fill", "#B5D3E7")
+    .attr("fill", lightBlue)
     .attr("stroke", "black");
 
   grid
@@ -46,4 +47,20 @@ export function main() {
     .attr("fill", "orange")
     .attr("font-size", 13.5)
     .attr("font-weight", 700);
+
+  function handleClick(d, i) {
+    let selection = d3.select(this);
+    let selectSq = selection.select("rect");
+    if (selection.attr("class") === "design-square unfilled") {
+      // toggle to filled
+      selectSq.attr("fill", "blue");
+      selection.attr("class", "design-square filled");
+    } else if (selection.attr("class") === "design-square filled") {
+      // toggle back to unfilled
+      selectSq.attr("fill", lightBlue);
+      selection.attr("class", "design-square unfilled");
+    }
+  }
+
+  grid.on("click", handleClick);
 }
